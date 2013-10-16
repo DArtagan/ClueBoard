2c6b51237a3c8f8a5a565fc2fa3ed54fd7e861b5
Spencer Kerr <skerr@mines.edu>
2013-10-16 17:49:35 -0600

    BadConfigFormatException spacing changed

--- src/boardClasses/BadConfigFormatException.java
+++ src/boardClasses/BadConfigFormatException.java
@@ -1,16 +1,16 @@
 package boardClasses;
 
 public class BadConfigFormatException extends Exception {
-	
-	  public BadConfigFormatException(String message) {
-	    super(message);
-	  }
-	  public BadConfigFormatException() {
-	    super("Error with files");
 
-	  }
-	  public String toString() {
-	   return "Error with files ";
-	  }
+	public BadConfigFormatException(String message) {
+		super(message);
+	}
 
+	public BadConfigFormatException() {
+		super("Error with files.");
+	}
+
+	public String toString() {
+		return "Error with files.";
+	}
 }
