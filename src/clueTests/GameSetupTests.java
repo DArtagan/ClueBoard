19e4d8ab5207939c268b1b7a6034c30793d66531
george <rpubaddr0@gmail.com>
2013-10-19 17:32:12 -0600

    Add method stubs for SetupTests.

--- src/clueTests/GameSetupTests.java
+++ src/clueTests/GameSetupTests.java
@@ -1,6 +1,6 @@
 package clueTests;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -12,7 +12,17 @@ public class GameSetupTests {
 	}
 
 	@Test
-	public void test() {
+	public void testLoadPeople() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLoadCards() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDealCards() {
 		fail("Not yet implemented");
 	}
 
