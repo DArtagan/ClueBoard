5fc0fde36565150a17292d7ebe6c972193ca0efd
William Weiskopf <william@weiskopf.me>
2013-10-19 17:32:20 -0600

    Blank card class.

---
+++ src/clue/Card.java
@@ -0,0 +1,27 @@
+package clue;
+
+public class Card {
+	public enum CardType {ROOM, WEAPON, PERSON};
+	public String Name;
+
+	public Card() {
+
+	}
+
+	public String getName() {
+		return null;
+	}
+
+	public CardType getCardType() {
+		return null;
+	}
+
+	public void setName() {
+
+	}
+
+	public void setCardType() {
+
+	}
+
+}
