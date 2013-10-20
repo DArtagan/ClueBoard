8f3f2b9e56873680e5db17ccbe8dce7b91c3449b
george <rpubaddr0@gmail.com>
2013-10-19 18:28:50 -0600

    Added UML method stubs to ComputerPlayer.

--- src/clue/ComputerPlayer.java
+++ src/clue/ComputerPlayer.java
@@ -1,9 +1,23 @@
 package clue;
 
+import java.util.HashSet;
+
 public class ComputerPlayer extends Player {
+	private int lastRoomVisited;
 
 	public ComputerPlayer(String player, String color, int index) {
 		super(player, color, index);
 	}
 
+	public void pickLocation(HashSet<BoardCell> targets) {
+
+	}
+
+	public void createSuggestion() {
+
+	}
+
+	public void updateSeen(Card seen) {
+
+	}
 }
