5d4c2ef0f71b65ffb9b74543030cd046567e20d0
george <rpubaddr0@gmail.com>
2013-10-19 18:06:18 -0600

    Writing failing Player tests.

--- src/clueTests/GameSetupTests.java
+++ src/clueTests/GameSetupTests.java
@@ -1,19 +1,38 @@
 package clueTests;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import clue.ClueGame;
+import clue.ComputerPlayer;
+
 public class GameSetupTests {
+	private ClueGame clueGame;
 
 	@Before
 	public void setUp() throws Exception {
+		clueGame = new ClueGame();
+		clueGame.loadConfigFiles();
 	}
 
 	@Test
 	public void testLoadPeople() {
-		fail("Not yet implemented");
+		// Valid players
+		ComputerPlayer mrgreen = new ComputerPlayer("Mr. Green");
+		ComputerPlayer mustard = new ComputerPlayer("Colonel Mustard");
+		ComputerPlayer scarlet = new ComputerPlayer("Miss Scarlet");
+		// Not a valid player
+		ComputerPlayer player0 = new ComputerPlayer("Player Zero");
+
+		assertTrue(clueGame.getPlayers().contains(mrgreen));
+		assertTrue(clueGame.getPlayers().contains(mustard));
+		assertTrue(clueGame.getPlayers().contains(scarlet));
+		assertFalse(clueGame.getPlayers().contains(player0));
+		assertTrue(clueGame.getPlayers().size() == 6);
 	}
 
 	@Test
