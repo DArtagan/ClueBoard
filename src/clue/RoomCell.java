c432d905a81c479e9e1108cc7b5a6bd91cc4f144
george <rpubaddr0@gmail.com>
2013-10-30 23:08:35 -0600

    Added room names and walls.

--- src/clue/RoomCell.java
+++ src/clue/RoomCell.java
@@ -12,11 +12,11 @@ public class RoomCell extends BoardCell {
 	RoomCell(int row, int col, char initial, char direction) {
 		super(row, col, initial);
 		switch (direction) {
-		case 'U': doorDirection = DoorDirection.UP; break;
-		case 'D': doorDirection = DoorDirection.DOWN; break;
-		case 'L': doorDirection = DoorDirection.LEFT; break;
-		case 'R': doorDirection = DoorDirection.RIGHT; break;
-		default: doorDirection = DoorDirection.NONE; break;
+			case 'U': doorDirection = DoorDirection.UP; break;
+			case 'D': doorDirection = DoorDirection.DOWN; break;
+			case 'L': doorDirection = DoorDirection.LEFT; break;
+			case 'R': doorDirection = DoorDirection.RIGHT; break;
+			default: doorDirection = DoorDirection.NONE; break;
 		}
 	}
 
@@ -47,9 +47,9 @@ public class RoomCell extends BoardCell {
 		int size = GUIBoard.CELL_SIZE;
 		g.setColor(Color.decode("0x073642"));
 		if (isDoorway()) {
-			g.fill3DRect(size*col, size*row, size, size, true);
+			g.fillRect(size*col, size*row, size, size);
 		} else {
-			g.fill3DRect(size*col, size*row, size, size, true);
+			g.fillRect(size*col, size*row, size, size);
 		}
 		g.setColor(Color.RED);
 		if (doorDirection == DoorDirection.UP) {
@@ -61,18 +61,18 @@ public class RoomCell extends BoardCell {
 		} else if (doorDirection == DoorDirection.LEFT) {
 			g.drawLine(col*size, row*size, col*size, row*size+size);
 		}
-		// Walls
+		// Draw walls
 		BoardCell cell;
 		LinkedList<Point> myCorners = new LinkedList<Point>();
 		LinkedList<Point> otherCorners = new LinkedList<Point>();
-		System.out.println(board.calcIndex(row, col));
-		for (Integer cellIndex : board.getAdjList(board.calcIndex(row, col))) {
+
+		for (Integer cellIndex : board.getAdjCells(board.calcIndex(row, col))) {
 			if ((cell = board.getCellAt(cellIndex)).isRoom()) {
 				if (cell.getInitial() != getInitial()) {
-					for (int i = 0; i < 1; ++i) {
-						for (int j = 0; j < 1; ++j) {
-							myCorners.add(new Point(col*i + col, row*j + row));
-							otherCorners.add(new Point(cell.col*i + cell.col, cell.row*j + cell.row));
+					for (int i = 0; i <= 1; ++i) {
+						for (int j = 0; j <= 1; ++j) {
+							myCorners.add(new Point(i + col, j + row));
+							otherCorners.add(new Point(i + cell.col, j + cell.row));
 						}
 					}
 					LinkedList<Point> matchingCorners = new LinkedList<Point>(myCorners);
@@ -81,7 +81,10 @@ public class RoomCell extends BoardCell {
 							matchingCorners.remove(point);
 						}
 					}
-					g.drawLine(matchingCorners.getFirst().x*size, matchingCorners.getFirst().y*size, matchingCorners.getLast().x*size, matchingCorners.getLast().y*size+size);
+					otherCorners.clear();
+					myCorners.clear();
+					g.setColor(Color.BLACK);
+					g.drawLine(matchingCorners.getFirst().x*size, matchingCorners.getFirst().y*size, matchingCorners.getLast().x*size, matchingCorners.getLast().y*size);
 				}
 			}
 		}
